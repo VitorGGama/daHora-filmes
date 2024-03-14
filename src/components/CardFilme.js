3fa5b97290405fa3219b890781a5c61aa963da12
Vitor <128196980+VitorGGama@users.noreply.github.com>
2024-03-14 10:42:22 -0300

    verificacao alerta e registro

--- src/components/CardFilme.js
+++ src/components/CardFilme.js
@@ -1,4 +1,12 @@
-import { Alert, Image, Pressable, StyleSheet, Text, View } from "react-native";
+import {
+  Alert,
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  Vibration,
+  View,
+} from "react-native";
 import imagemAlternativa from "../../assets/images/foto-alternativa.jpg";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -18,13 +26,32 @@ export default function CardFilme({ filme }) {
       com o nome indocado (@favoritosvitor). Existindo, ele é carregado para a 
       const filmesFavoritos. Se não existir, será criado posteriomente. */
       const filmesFavoritos = await AsyncStorage.getItem("@favoritosvitor");
+
       /* 2) Verificar/criar uma lista de filmes favoritos (dados).
       Se filmesFavoritos existir (ou seja, tem dados no storage), pegamos
       estes dados (strings) e convertemos em  objeto (JSON.parse). Caso contrário,
       listaDeFilmes será um array vazio. */
       const listaDeFilmes = filmesFavoritos ? JSON.parse(filmesFavoritos) : [];
-      /* 3) Verificar se já tem algum filme na lista */
+
+      /* 3) Verificar se já tem algum filme na lista 
+      Usamos a função some() para avaliar se o ID do filme dentro da listaDeFilmes é o mesmo de um filme exibido na tela do app (nos Cards). Se for, retorna TRUE indicando que o filme já foi salvo
+      em algum momento. Caso contrário, retorna FALSE indicando que o
+      filme ainda não foi salvo. */
+      const jaTemFilme = listaDeFilmes.some((filmeNaLista) => {
+        return filmeNaLista.id === filme.id;
+      });
+
       /* 4) Se o filme não estiver na lista, então vamos coloca-lo */
+
+      /* 4.1) Se já tem filme, avisaremos ao usuário*/
+      if (jaTemFilme) {
+        Alert.alert("Ops!", "Você já salvou este filme!");
+        Vibration.vibrate();
+        return;
+      }
+      /* 4.2) Senão, vamos colocar na lista */
+      listaDeFilmes.push(filme);
+
       /* 5) Usamos o AsyncStorage para gravar no armazenamento offline 
       do dispositivo */
     } catch (error) {
