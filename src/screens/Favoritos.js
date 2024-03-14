f5b67094542f10c822e72850fea5df17cf0161db
Vitor <128196980+VitorGGama@users.noreply.github.com>
2024-03-14 11:42:12 -0300

    listagem dos filmes favoritos

--- src/screens/Favoritos.js
+++ src/screens/Favoritos.js
@@ -1,14 +1,58 @@
 // src/screens/Sobre.js
-import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  Alert,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import SafeContainer from "../components/SafeContainer";
 
+import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Ionicons } from "@expo/vector-icons";
+
 export default function Favoritos() {
+  /* State para registrar os dados carregados do storage*/
+  const [listaFavoritos, setListaFavoritos] = useState([]);
+
+  /* useEffect será disparado assim que o usário entrar
+  na tela Favoritos (portanto, somente uma vez) */
+  useEffect(() => {
+    const carregarFavoritos = async () => {
+      try {
+        const dados = await AsyncStorage.getItem("@favoritosvitor");
+        if (dados) {
+          setListaFavoritos(JSON.parse(dados));
+        }
+      } catch (error) {
+        console.error("Erro ao carregar os dados: " + error);
+        Alert.alert("Erro", "Erro ao carregar os dados");
+      }
+    };
+    carregarFavoritos();
+  }, []);
+
+  console.log(listaFavoritos);
   return (
     <SafeContainer>
       <View style={estilos.subContainer}>
         <View style={estilos.viewFAvoritos}>
-          <Text style={estilos.texto}>Favoritos </Text>
+          <Text style={estilos.texto}>Quantidade:{listaFavoritos.length} </Text>
+          <Pressable style={estilos.botao}>
+            <Text style={estilos.textoBotao}>
+              <Ionicons name="trash-outline" size={16} />
+              Excluir favoritos
+            </Text>
+          </Pressable>
         </View>
+
+        <ScrollView showsVerticalScrollIndicator={false}>
+          {listaFavoritos.map((filme) => {
+            return <Text key={filme.id}>{filme.title}</Text>;
+          })}
+        </ScrollView>
       </View>
     </SafeContainer>
   );
