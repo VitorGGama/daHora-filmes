e4ac18ad0c065e2437246f6a0bbf4d239d35ca48
Vitor <128196980+VitorGGama@users.noreply.github.com>
2024-03-14 09:18:39 -0300

    funcionalidade de Favoritos

---
+++ src/screens/Favoritos.js
@@ -0,0 +1,26 @@
+// src/screens/Sobre.js
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import SafeContainer from "../components/SafeContainer";
+
+export default function Favoritos() {
+  return (
+    <SafeContainer>
+      <View style={estilos.subContainer}>
+        <View style={estilos.viewFAvoritos}>
+          <Text style={estilos.texto}>Favoritos </Text>
+        </View>
+      </View>
+    </SafeContainer>
+  );
+}
+
+const estilos = StyleSheet.create({
+  subContainer: {
+    flex: 1,
+    padding: 16,
+    width: "100%",
+  },
+  texto: {
+    marginVertical: 8,
+  },
+});
