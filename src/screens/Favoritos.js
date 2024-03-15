b80f9bd09aa0ec9fb8b0f16373f6474157563e37
Vitor <128196980+VitorGGama@users.noreply.github.com>
2024-03-15 08:55:28 -0300

    estilos css excluir

--- src/screens/Favoritos.js
+++ src/screens/Favoritos.js
@@ -51,14 +51,14 @@ export default function Favoritos() {
         <ScrollView showsVerticalScrollIndicator={false}>
           {listaFavoritos.map((filme) => {
             return (
-              <View style={estilos.item}>
+              <View key={filme.id} style={estilos.item}>
                 <Pressable style={estilos.botaoFilme}>
                   <Text style={estilos.titulo}>{filme.title}</Text>
                 </Pressable>
 
                 <Pressable style={estilos.botaoExcluir}>
                   <Text>
-                    <Ionicons name="trash" size={16} />
+                    <Ionicons color="white" name="trash" size={16} />
                   </Text>
                 </Pressable>
               </View>
@@ -93,4 +93,20 @@ const estilos = StyleSheet.create({
     borderRadius: 4,
   },
   textoBotao: { color: "red" },
+  item: {
+    padding: 8,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#eee8fc",
+    marginBottom: 8,
+    borderRadius: 4,
+    alignItems: "center",
+  },
+  botaoFilme: { flex: 1 },
+  titulo: { fontSize: 14 },
+  botaoExcluir: {
+    backgroundColor: "darkred",
+    padding: 4,
+    borderRadius: 4,
+  },
 });
